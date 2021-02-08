41f4fdc2db45dd4a4498ad9d662bdbab886d106a
Johannes Schöpp <schoepp@rz.uni-frankfurt.de>
2021-02-08 15:59:29 +0100

    Build: Added creation of the directory "out"

--- build.ps1
+++ build.ps1
@@ -4,6 +4,11 @@ $out = Join-Path $Here "out"
 $bin = Join-Path $Here "bin"
 $ModulePath = Join-Path $Here $ModuleName
 
+if (Test-Path $out) {
+    Remove-Item $out -Recurse -Verbose
+}
+New-Item $out -ItemType Directory
+
 Copy-Item -Path $ModulePath\* -Destination $out -Recurse
 $Module = Test-ModuleManifest -Path (Join-path -Path $out -ChildPath "$ModuleName.psd1")
 nuget.exe pack "$ModuleName.nuspec" -Version $Module.Version -BasePath $out -outputdirectory $bin
